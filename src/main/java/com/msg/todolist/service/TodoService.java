89760d34ec08883f9d47b0ed3a3ccc868eabfc4f
Mami <gultekinmsg@gmail.com>
2020-12-27 16:37:50 +0300

    Optimized repository requests and Refactored TodoService

--- src/main/java/com/msg/todolist/service/TodoService.java
+++ src/main/java/com/msg/todolist/service/TodoService.java
@@ -1,44 +1,38 @@
 package com.msg.todolist.service;
 
 import com.msg.todolist.entity.Todo;
-import com.msg.todolist.entity.User;
 import com.msg.todolist.model.TodoRequest;
 import com.msg.todolist.model.TodoResponse;
 import com.msg.todolist.repository.TodoRepository;
 import com.msg.todolist.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class TodoService {
 
     private final TodoRepository todoRepository;
-    private final UserRepository userRepository;
     private final UserDetailService userDetailService;
 
     @Autowired
     public TodoService(TodoRepository todoRepository, UserRepository userRepository, UserDetailService userDetailService) {
         this.todoRepository = todoRepository;
-        this.userRepository = userRepository;
         this.userDetailService = userDetailService;
     }
 
     public List<TodoResponse> findAll() {
-        if (todoRepository.findAllByOrderByDateTimeDesc().isEmpty()) {
+        if (todoRepository.findAllByUserIdOrderByDateTimeDesc(userDetailService.findCurrentUser().getId()).isEmpty()) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "empty todo list");
         }
         List<TodoResponse> todoResponseList = new ArrayList<>();
-        List<Todo> items = todoRepository.findAllByOrderByDateTimeDesc();
+        List<Todo> items = todoRepository.findAllByUserIdOrderByDateTimeDesc(userDetailService.findCurrentUser().getId());
         for (Todo item : items) {
-            if (item.getUser().getId().equals(userDetailService.findCurrentUser().getId())){
                 TodoResponse model = new TodoResponse();
                 model.setId(item.getId());
                 model.setName(item.getName());
@@ -46,7 +40,6 @@ public class TodoService {
                 model.setDateTime(item.getDateTime());
                 model.setUserId(item.getUser().getId());
                 todoResponseList.add(model);
-            }
         }
         return todoResponseList;
     }
@@ -61,34 +54,15 @@ public class TodoService {
     }
 
     public void deleteTodo(Long todoId) {
-        Optional<Todo> todos = todoRepository.findById(todoId);
-        if (todos.get().getUser().getId().equals(userDetailService.findCurrentUser().getId())){
-            checkTodoExists(todoId);
-            todoRepository.deleteById(todoId);
-        }else {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Not authorized");
-        }
+        Todo todoToChange = todoRepository.findByIdAndUserId(todoId,userDetailService.findCurrentUser().getId());
+        todoRepository.delete(todoToChange);
+
     }
 
     public void toggleTodo(Long todoId) {
-        Optional<Todo> todos = todoRepository.findById(todoId);
-        if (todos.get().getUser().getId().equals(userDetailService.findCurrentUser().getId())){
-            Todo todo = todoRepository.findById(todoId)
-                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "item not exist"));
-            todo.setCompleted(!todo.getCompleted());
-            todoRepository.save(todo);
-        }else {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Not authorized");
-        }
-
+        Todo todoToChange = todoRepository.findByIdAndUserId(todoId,userDetailService.findCurrentUser().getId());
+        todoToChange.setCompleted(!todoToChange.getCompleted());
+        todoRepository.save(todoToChange);
     }
-
-    private void checkTodoExists(Long todoId) {
-        if (todoRepository.findById(todoId).isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "item not exist");
-        }
-    }
-
-
 
 }
